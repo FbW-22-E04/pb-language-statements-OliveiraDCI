d20ad9e513190f3bd76a75e1d8c6b7a49f704595
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-06-30 13:02:59 +0200

    Exercise concluded.

--- solution.js
+++ solution.js
@@ -1,1 +1,9 @@
 console.log("Hello World");
+console.log('This is my first js file');
+
+// 1
+console.log("Hello World";
+
+console.log("Hello World");
+// The statement does not print. There is an error message printed in the console: 'SyntaxError: missing ) after argument list
+
